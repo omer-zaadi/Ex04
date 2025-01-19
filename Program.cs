e17af754d2dd81a6033a5ee38f0ab903369cf3a4
omer-zaadi <omerzadi2014@gmail.com>
2025-01-19 18:45:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Ex04.Menus.Events;
+
+class Program
+{
+    static void Main()
+    {
+        var mainMenu = new MainMenu();
+
+        var lettersAndVersionMenu = new MenuItem("Letters and Version");
+        //lettersAndVersionMenu.SubItems.Add(new MenuItem("Show Version", () => Console.WriteLine("App Version: 25.1.4.5480")));
+        lettersAndVersionMenu.SubItems.Add(new MenuItem("Show Version", () =>
+        {
+            Console.WriteLine("App Version: 25.1.4.5480");
+        }));
+        lettersAndVersionMenu.SubItems.Add(new MenuItem("Count Lowercase Letters", () =>
+        {
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+            int lowercaseCount = input.Count(char.IsLower);
+            Console.WriteLine($"There are {lowercaseCount} lowercase letters.");
+        }));
+
+        var dateTimeMenu = new MenuItem("Show Current Date/Time");
+        
+        dateTimeMenu.SubItems.Add(new MenuItem("Show Current Time", () =>
+        {
+            Console.WriteLine($"Current Time: {DateTime.Now:HH:mm:ss}");
+        }));
+       
+        dateTimeMenu.SubItems.Add(new MenuItem("Show Current Date", () =>
+        {
+            Console.WriteLine($"Current Date: {DateTime.Now:dd/MM/yyyy}");
+        }));
+
+
+
+        mainMenu.AddMenuItem(lettersAndVersionMenu);
+        mainMenu.AddMenuItem(dateTimeMenu);
+
+        mainMenu.Show();
+    }
+}
