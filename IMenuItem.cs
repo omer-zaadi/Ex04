10a7cb956453a1fa79dba9733cdbfed3d8f26e1d
omer-zaadi <omerzadi2014@gmail.com>
2025-01-19 18:44:19 +0200

    Add files via upload

---
+++ IMenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex04.Menus.Interfaces
+{
+    public  interface IMenuItem
+    {
+        string Title { get; }
+        void Execute(); // פעולה שתבוצע כאשר בוחרים את הפריט
+        bool HasSubItems { get; }
+        List<IMenuItem> SubItems { get; }
+    }
+}
--- MenuItem.cs
+++ MenuItem.cs
@@ -1,34 +1,12 @@
-﻿using Ex04.Menus.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Ex04.Menus.Events
+namespace Ex04.Menus.Interfaces
 {
-    public class MenuItem : Ex04.Menus.Interfaces.IMenuItem
+    internal class MenuItem
     {
-        public string Title { get; }
-        public Action ExecuteAction { get; set; }
-        public List<IMenuItem> SubItems { get; private set; }
-        public bool HasSubItems => SubItems.Count > 0;
-       
-
-        public MenuItem(string title, Action executeAction = null)
-        {
-            Title = title;
-            ExecuteAction = executeAction;
-            SubItems = new List<IMenuItem>();
-        }
-
-        public void Execute()
-        {
-            if (ExecuteAction != null)
-            {
-                ExecuteAction.Invoke();
-            }
-            else
-            {
-                Console.WriteLine($"Executing menu: {Title}");
-            }
-        }
     }
 }
